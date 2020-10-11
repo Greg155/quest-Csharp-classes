9a239f0621c67a276e57b89af379e8d2b49c2dc3
Greg155 <gregakhun@gmail.com>
2020-10-11 11:28:14 +0200

    C# quest fighting video game

---
+++ FighterVideoGame/FighterVideoGame/Character.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FighterVideoGame
+{
+    class Character
+    {
+        public string name;
+        public int healthPoint;
+        public int strength;
+        public int defense;
+
+        public Character(string name, int healthPoint, int strength, int defense)
+        {
+            this.name = name;
+            this.healthPoint = healthPoint;
+            this.strength = strength;
+            this.defense = defense;
+        }
+
+        public Boolean IsAlive()
+        {
+            if (healthPoint <= 0)
+            {
+                Console.WriteLine(name + " Ko");
+                return false;
+            }else{
+                Console.WriteLine(name + " Still alive!");
+                return true;
+            }
+            
+        }
+
+        public int Attack(int enemyAtk)
+        {
+            healthPoint = healthPoint - (enemyAtk - defense);
+            if(healthPoint<= 0)
+            {
+                healthPoint = 0;
+            }
+            return healthPoint;
+        }
+
+    }
+}
---
+++ FighterVideoGame/FighterVideoGame/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FighterVideoGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Character James = new Character("James", 100, 55, 5);
+            Character Lizzie = new Character("Lizzie", 100, 50, 15);
+
+            do
+            {
+                James.Attack(Lizzie.strength);
+                Console.WriteLine("Lizzie attack James");
+                Console.WriteLine(James.name + " has " + James.healthPoint + "HP left!");
+                if (!James.IsAlive())
+                {
+                    Console.WriteLine("James Fainted !");
+                    Console.WriteLine("Lizzie wins !");
+                    break;
+                }
+
+                Lizzie.Attack(James.strength);
+                Console.WriteLine("James attack Lizzie");
+                Console.WriteLine(Lizzie.name + " has " + Lizzie.healthPoint + "HP left!");
+                if (!Lizzie.IsAlive())
+                {
+                    Console.WriteLine("Lizzie Fainted !");
+                    Console.WriteLine("James wins !");
+                    break;
+                }
+
+            } while (James.IsAlive() && Lizzie.IsAlive());
+
+
+        }
+    }
+}
